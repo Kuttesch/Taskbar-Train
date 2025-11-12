8a751b90acf6af117c82a5be87ac3948dfc50e7e
Kuttesch <moritzkuttesch@outlook.de>
2025-11-12 18:58:23 +0100

    initial commit

---
+++ src/example.c
@@ -0,0 +1,12 @@
+/**
+ * @file example.c
+ * @brief Implements the example public API.
+ * @date 2025-11-09
+ * @version 1.0
+ */
+
+#include "example.h"
+
+int internal_process(int i) {
+    return i;
+}
---
+++ test/test_example.c
@@ -0,0 +1,18 @@
+/**
+ * @file test_public_api.c
+ * @brief Example Unity test for the public API.
+ * @date 2025-11-09
+ * @version 1.0
+ */
+
+#include "unity.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+int main(void)
+{
+    UNITY_BEGIN();
+
+    return UNITY_END();
+}
